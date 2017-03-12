8c955dc1fc69141ab515b73b04bcd18e12b3836f
Harsh Vardhan <harshvd95@gmail.com>
2017-03-12 13:36:34 +0530

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name studyPortalApp
+ * @description
+ * # studyPortalApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('studyPortalApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name studyPortalApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the studyPortalApp
+ */
+angular.module('studyPortalApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
